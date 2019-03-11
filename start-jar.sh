f7d0354484566e75b10b1b9dd016a982b7eff16c
root <455083501@qq.com>
2019-03-11 14:33:18 +0800

    修改bug

--- start-jar.sh
+++ start-jar.sh
@@ -1,11 +1,14 @@
 #~/bin/sh
-rm -rf nohup.out
+path=`pwd`
+filepath="${path}/nohup.out"
+
+rm -rf ${filepath}
 pid=`ps -ef|grep $1|head -1|awk '{printf $2}'`
 echo $pid
 kill -9 $pid
 nohup java -jar $1 &
 
-until [ -e 'nohup.out' ]
+until [ -e ${filepath} ]
 do 
 tail -f nohup.out
 done
